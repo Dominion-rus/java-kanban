39fa0f22e354617297c3cac537154b1bb93150b9
Dominion_rus <fatal1ty2013@yandex.ru>
2024-08-26 19:09:21 +0300

    push project files

---
+++ src/main/java/org/practicum/Task.java
@@ -0,0 +1,63 @@
+package org.practicum;
+
+import java.util.Objects;
+
+
+/**
+ * @author vsmordvincev
+ */
+public class Task {
+    protected int id;
+    protected String title;
+    protected String description;
+    protected Status status;
+
+      public Task(String title, String description, Status status) {
+        this.title = title;
+        this.description = description;
+        this.status = status;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+      public String toString() {
+        return "Task{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ", status=" + status +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Task task = (Task) o;
+        return id == task.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
+
