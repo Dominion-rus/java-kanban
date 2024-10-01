13758f71e1258ba883a945ea53b96147549bb74e
Dominion_rus <fatal1ty2013@yandex.ru>
2024-10-01 15:41:11 +0300

    fix style error

--- src/main/java/ru/yandex/practicum/service/HistoryManager.java
+++ src/main/java/ru/yandex/practicum/service/HistoryManager.java
@@ -5,8 +5,11 @@ import ru.yandex.practicum.model.Task;
 import java.util.List;
 
 public interface HistoryManager {
+
     void add(Task task);
+
     void remove(int id);
+
     List<Task> getHistory();
 
 }
